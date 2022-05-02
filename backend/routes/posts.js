d58904510eaa5f2fca245e68fb5fa860415ce6d8
n-roberge <58243482+n-roberge@users.noreply.github.com>
2022-05-02 17:56:55 -0500

    finished posts endpoints

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,12 +3,47 @@ const express = require("express");
 const router = express.Router();
 
 //GET all posts
-
-
+router.get("/", async (req, res) => {
+    try {
+      // console.log(req.post);
+      const posts = await Post.find();
+      if (!posts) return res.status(400).send(`No posts available`);
+      return res.send(posts);
+    } catch (error) {
+      return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+  });
 
 //GET post by user
+router.get("/:id", async (req, res) => {
+    try {
+        let post = await Post.findById(req.params.id);
+        if (!post) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        return res.send(post);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
 
 //PUT update a post
+router.put("/:id", async (req, res) => {
+    try {
+        let post = await Post.findByIdAndUpdate(
+            req.params.id,
+            {
+                name: req.body.name,
+                body: req.body.body,
+                like: req.body.like,
+                dislike: req.body.dislike,
+            },
+            {new:true}
+        );
+        if (!post) return res.status(400).send(`Post with id "${req.params.id}" does not exist!`);
+        return res.send(post);
+    } catch (error) {
+        return res.status(500).send(`Internal Server Error: ${error}`);
+    }
+});
 
 //DELETE a post
 //http://localhost:3011/api/posts/
@@ -16,7 +51,7 @@ router.delete("/:id", async (req, res) => {
     try {
         let post = await Post.findByIdAndDelete(req.params.id);
 
-        if (!post) return res.status(400).send(`Comment with id "${req.params.id}" does not exist!`);
+        if (!post) return res.status(400).send(`Post with id "${req.params.id}" does not exist!`);
         return res.send(post);
     } catch (error) {
         return res.status(500).send(`Internal Server Error: ${error}`);
