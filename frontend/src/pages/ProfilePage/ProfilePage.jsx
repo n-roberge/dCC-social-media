2312f6c2294beed3ab5fc88856ca605e6b6444d8
William Hagan <williamhagan1728@yahoo.com>
2022-05-06 17:02:34 -0700

    add profile page and logic to profile.jsk/css

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,36 +1,29 @@
-import "./ProfilePage.css"
+import "./ProfilePage.css";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 
 const ProfilePage = (props) => {
   const { user } = useContext(AuthContext);
-  return (
-    <div>
-    <div className="Name">{user.name}</div>
-    <div className="Bio"></div>
-    <div className="Email"></div>
-    <div></div>
-    </div>
-  );
+  const [edit, setEdit] = useState(false);
+
+  if (edit) {
+  return ( <div>
+    <div className="Name">Edit Name: {user.name}</div>
+    <div className="About">Edit Bio: {user.about}</div>
+    <div className="Email">Edit Email: {user.email}</div>
+    <button onClick={setEdit(!edit)}>Edit</button>
+  </div>)
+  } else {
+    return (
+      <div>
+        <div className="Name">Name: {user.name}</div>
+        <div className="About">Bio: {user.about}</div>
+        <div className="Email">Email: {user.email}</div>
+        <button >Edit</button>
+      </div>
+    );
+  }
 };
 
 export default ProfilePage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
