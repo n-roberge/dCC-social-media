6205711cf9dd9ce03131e811a54894539ff50ec7
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2022-05-08 20:13:21 -0500

    Added hooks for the put request in the ProfilePage

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,39 +1,68 @@
 import "./ProfilePage.css";
-import axios from "axios"
+import axios from "axios";
 
 import { useContext, useState } from "react";
 import AuthContext from "../../context/AuthContext";
 
 const ProfilePage = (props) => {
   const { user } = useContext(AuthContext);
+
   const [edit, setEdit] = useState(false);
-  const baseURL = "http://localhost:3011/api/users/"
-
+  const [name, setName] = useState(user.name);
+  const [bio, setBio] = useState("");
+  const [email, setEmail] = useState("");
+  const baseURL = "http://localhost:3011/api/users/";
+  const headers = {
+    Authorization: 'Bearer ' + localStorage.getItem("token"),
+  };
   function switchEdit() {
-  setEdit(!edit)
+    setEdit(!edit);
+    console.log(headers);
   }
-
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(user);
     const payLoad = {
       user: user._id,
-     
+      aboutme: bio,
     };
-    axios.put(baseURL, payLoad).then((response) => {
+    axios.put(baseURL + user._id, headers, payLoad).then((response) => {
       console.log(response.data);
     });
-    switchEdit()
+    switchEdit();
   }
 
   if (edit) {
-  return ( <form onSubmit={handleSubmit}>
-    <div className="Name">Edit Name: {user.name}</div>
-    <div className="About">Edit Bio: {user.about}</div>
-    <div className="Email">Edit Email: {user.email}</div>
-    
-  </form>)
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder={user.name}
+          name="name"
+          title="name"
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          placeholder={user.about}
+          name="bio"
+          title="bio"
+          type="text"
+          value={bio}
+          onChange={(event) => setBio(event.target.value)}
+        />
+        <input
+          placeholder={user.email}
+          name="email"
+          title="email"
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input className="button" type="submit" value="Submit" />
+        <button onClick={switchEdit}>Cancel</button>
+      </form>
+    );
   } else {
     return (
       <div>
